881039bba02aeacfa628a3056f1671b761d2106c
lhmiao <2696254238@qq.com>
2018-10-08 15:25:30 +0800

    完成图片自动滚动

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -3,23 +3,30 @@ function Carousel (option) {
   this.imgURLs = option.imgURLs;
   this.hasBtns = option.hasBtns || true;
   this.isMoveing = false;
+  this.timeId = null;
+  this.interval = option.interval || 5000;
   this.container = document.querySelector(this.el);
   this.init();
 }
 
 Carousel.prototype = {
   init: function () {
+    var that = this;
     this.imgList = this.createImgList();
+    // this.imgList.addEventListener('transitionend', function (e) {
+    //   that.isMoveing = false;
+    // }, false);
     if(this.hasBtns) {
       this.btnList = this.createBtnList();
     }
-    this.container.appendChild(this,imgList);
+    this.container.appendChild(this.imgList);
     this.container.appendChild(this.btnList);
-    var that = this;
-    setTimeout(function () {
+    window.addEventListener('load', function () {
       that.initImgInfo();
-      that.timeId = that.move(); 
-    }, 0);
+      that.initBtnList();
+      that.initContainer();
+      that.move(0);
+    }, false);
   },
   createImgList: function () {
     var imgListDiv = document.createElement('div');
@@ -34,12 +41,36 @@ Carousel.prototype = {
     var btnListDiv = document.createElement('div');
     btnListDiv.className = 'carousel-btn-list';
     btnListDiv.innerHTML = this.imgURLs.reduce(function (pre, cur, curIndex) {
-      if (curIndex === 0) {
-        return pre + '<div class="active-btn"></div>';
-      } else {
-        return pre + '<div></div>';
-      }
+      return pre + '<div ' + 'data-index="' + curIndex + '"></div>';
     }, '');
     return btnListDiv;
+  },
+  initImgInfo: function () {
+    this.imgNumber = this.imgList.querySelectorAll('img').length;
+    var img = this.imgList.querySelectorAll('img')[0];
+    this.imgWidth = img.clientWidth;
+    this.imgHeight= img.clientHeight;
+  },
+  initBtnList: function () {
+    var btns = this.btnList.querySelectorAll('div');
+    btns[0].className = 'active-btn';
+    var that = this;
+    this.btnList.addEventListener('click', function (e) {
+      clearTimeout(that.timeId);
+      that.move(e.target.dataset.index);
+    })
+  },
+  initContainer: function () {
+    this.container.style.overflow = 'hidden';
+    this.container.style.width = this.imgWidth + 'px';
+    this.container.style.height = this.imgHeight + 'px';
+  },
+  move: function (nextIndex) {
+    // if (this.isMoveing) {
+    //   return;
+    // }
+    this.imgList.style.marginLeft = - (this.imgWidth * nextIndex) + 'px';
+    var that = this;
+    this.timeId = setTimeout(this.move.bind(this, (++nextIndex) % this.imgNumber), this.interval);
   }
 };
